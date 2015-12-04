ee9181fa559248ee41f91c40a1e3f6e8dcb1ae8f
Jie Hou <jie@uber.com>
2015-12-04 02:30:21 -0800

    finish day 4 part 1

--- app/src/main/java/com/codepath/instagram/fragments/PostsFragment.java
+++ app/src/main/java/com/codepath/instagram/fragments/PostsFragment.java
@@ -6,6 +6,7 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -32,6 +33,7 @@ import cz.msebera.android.httpclient.Header;
 public class PostsFragment extends Fragment {
 
     private List<InstagramPost> posts;
+    private SwipeRefreshLayout swipeContainer;
 
     public static PostsFragment newInstance() {
         PostsFragment fragment = new PostsFragment();
@@ -63,6 +65,23 @@ public class PostsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_posts, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        swipeContainer = (SwipeRefreshLayout) getView().findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchPosts();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
     private void fetchPosts() {
         InstagramClient client = MainApplication.getRestClient();
         client.getFeed(new JsonHttpResponseHandler() {
@@ -74,12 +93,14 @@ public class PostsFragment extends Fragment {
                 rvPosts.setAdapter(adapter);
                 rvPosts.setLayoutManager(new LinearLayoutManager(getActivity()));
                 rvPosts.addItemDecoration(new SimpleVerticalSpacerItemDecoration(24));
+
+                // deal with the case when fetch was triggered by swipe
+                swipeContainer.setRefreshing(false);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 Utils.makeToast("Please check your network", getActivity());
-                return;
             }
         });
     }
